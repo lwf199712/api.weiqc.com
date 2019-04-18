6b27c400915c594a7585c106da813d6f1a0e3d0e
lirong <18292800@qq.com.com>
2019-04-18 17:15:29 +0800

    文档更新

--- config/api.php
+++ config/api.php
@@ -2,9 +2,9 @@
 return [
     'tencent_marketing_api' => [
         'base_url'     => YII_ENV === 'prod' ? 'https://api.e.qq.com/v1.1/' : 'https://sandbox-api.e.qq.com/v1.1/',
-        'access_token' => 'ce50f5a67bde88c617d901e95b597639',
+        'access_token' => YII_ENV === 'prod' ? '635bcbdac7be8e86d5f99d95dd52b67a' : 'ce50f5a67bde88c617d901e95b597639',
         'api'          => [
-            'user_actions'     => [
+            'user_actions' => [
                 //上传用户行为数据
                 'add' => 'user_actions/add'
             ],
