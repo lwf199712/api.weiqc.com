5fc0c7859c159e678b39d342d297381be33e6ff8
lirong <18292800@qq.com.com>
2019-05-10 16:16:23 +0800

    fixbug

--- web/index.php
+++ web/index.php
@@ -1,6 +1,7 @@
 <?php
 
 // comment out the following two lines when deployed to production
+use app\common\utils\StringUtil;
 use yii\base\InvalidConfigException;
 
 defined('YII_DEBUG') or define('YII_DEBUG', false);
@@ -10,6 +11,18 @@ require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $config = require __DIR__ . '/../config/web.php';
+
+//跨域
+$httpOrigin = $_SERVER['HTTP_ORIGIN'] ?? '';
+if ($httpOrigin) {
+    $httpOrigin = stringUtil::cutOutLater($httpOrigin, 'https://');
+    $httpOrigin = stringUtil::cutOutFormer($httpOrigin, 'http://');
+    $httpOrigin = stringUtil::cutOutFormer($httpOrigin, '.');
+    if (in_array($httpOrigin, Yii::$app->params['cross_domain'], false)) {
+        header('Access-Control-Allow-Origin:' . $httpOrigin);
+        header('Access-Control-Allow-Headers:' . $httpOrigin);
+    }
+}
 
 try {
     (new yii\web\Application($config))->run();
