b504a0fa5d74649a040dc69f60833604a132676f
ctl <chentulinys@163.com>
2019-09-26 15:13:12 +0800

    'version'

---
+++ models/dataObject/ZhitongcheImgDo.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace app\models\dataObject;
+
+use Yii;
+use app\models\User;
+use yii\db\ActiveRecord;
+use yii\behaviors\AttributeBehavior;
+
+
+
+/**
+ * This is the model class for table "bm_zhitongche_img".
+ *
+ * @property int $id
+ * @property string $version 完成日期
+ * @property string $name 名称
+ * @property string $stylist 设计师
+ * @property string $picture_address 图片地址
+ * @property int $upload_time 上传时间
+ * @property int $audit_status 审核状态(0/待审核，1/通过，2/不通过)
+ * @property string $audit_opinion 审核意见
+ * @property string $auditor 审核人
+ * @property int $audit_time 审核时间
+ */
+class ZhitongcheImgDo extends ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'bm_zhitongche_img';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['version', 'name', 'stylist', 'picture_address'], 'required'],
+            [['upload_time', 'audit_status', 'audit_time'], 'integer'],
+            [['version', 'name', 'stylist', 'audit_opinion', 'auditor'], 'string', 'max' => 255],
+            [['picture_address'], 'string', 'max' => 1024],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'version' => 'Version',
+            'name' => 'Name',
+            'stylist' => 'Stylist',
+            'picture_address' => 'Picture Address',
+            'upload_time' => 'Upload Time',
+            'audit_status' => 'Audit Status',
+            'audit_opinion' => 'Audit Opinion',
+            'auditor' => 'Auditor',
+            'audit_time' => 'Audit Time',
+        ];
+    }
+}
