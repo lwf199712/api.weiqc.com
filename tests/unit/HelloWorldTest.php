3bbc0442a13dae78241a6c5b0c7c14ab257e82e7
zengzhuozhen <514551226@qq.com>
2019-10-16 10:30:13 +0800

    unit testing demo

--- tests/unit/HelloWorldTest.php
+++ tests/unit/HelloWorldTest.php
@@ -1,23 +1,47 @@
 <?php
 
-class HelloWorldTest extends \Codeception\Test\Unit
+use app\modules\v2\operateDept\domain\aggregate\DesignCenterAggregate;
+use Codeception\Test\Unit;
+use yii\di\Instance;
+
+class HelloWorldTest extends Unit
 {
     /**
-     * @var \UnitTester
+     * @var UnitTester
      */
     protected $tester;
-    
+
+    public $designCenter;
+
+
     protected function _before()
     {
+        $this->designCenter = Instance::ensure(DesignCenterAggregate::class);
     }
 
     protected function _after()
     {
     }
 
-    // tests
-    public function testSomeFeature()
+    /**
+     * @test
+     */
+    public function someFeature() : string
     {
-        codecept_debug(1);
+        $this->assertInstanceOf(DesignCenterAggregate::class,$this->designCenter);
+        return 'xxx';
     }
+
+    /**
+     * @test
+     * @depends someFeature
+     * @param string $params
+     */
+    public function otherFeature(string $params) : void
+    {
+        $this->assertEquals('xxx',$params);
+    }
+
+
+
 }
