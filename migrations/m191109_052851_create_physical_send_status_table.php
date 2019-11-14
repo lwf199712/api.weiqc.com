3db40850091cb770d099e2abac23aed0e4db72e5
luweifeng <1253811795@qq.com>
2019-11-14 15:58:39 +0800

    解决数据迁移错误

--- migrations/m191109_052851_create_physical_send_status_table.php
+++ migrations/m191109_052851_create_physical_send_status_table.php
@@ -15,7 +15,7 @@ class m191109_052851_create_physical_send_status_table extends Migration
         $this->createTable('{{%physical_send_status}}', [
             'id' => $this->primaryKey(),
             'recipients'        => $this->string()->null()->defaultValue('')->comment('收件人'),
-            'phone'             => $this->integer(11)->null()->defaultValue('')->comment('联系电话'),
+            'phone'             => $this->integer(11)->null()->defaultValue(0)->comment('联系电话'),
             'delivery_site'     => $this->string()->null()->defaultValue('')->comment('收货地址'),
             'tracking_number'   => $this->string()->notNull()->defaultValue('')->comment('快递单号'),
             'rp_id'             => $this->integer(11)->null()->defaultValue(0)->comment('置换订单'),
