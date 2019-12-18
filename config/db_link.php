9d0519691b5dbeea31921570178ea8de4b760f40
dengkai <yaodengkai1266@dingtalk.com>
2019-12-18 14:27:57 +0800

    修改UAC访问的URL

---
+++ config/db_link.php
@@ -0,0 +1,17 @@
+<?php
+
+use yii\db\Connection;
+
+return [
+    'class'       => Connection::class,
+    'charset'     => 'utf8',
+    'tablePrefix' => 'bm_',
+
+    'dsn'         => 'mysql:host=222.187.239.39;dbname=weiqc_dc',
+    'username'    => 'root',
+    'password'    => 'Fandow-root-1114'
+
+//    'dsn'         => 'mysql:host=192.168.1.200;dbname=weiqc_dc',
+//    'username'    => 'root',
+//    'password'    => 'root',
+];
